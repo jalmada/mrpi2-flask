c526ed488b3ea539705f1fdfbf748dd1ddd6c753
jalmada <jalmada.e@gmail.com>
2019-11-28 01:48:24 -0500

    Testing Video

--- server.py
+++ server.py
@@ -1,17 +1,23 @@
 #!flask/bin/python
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template, Response 
 from brightpilib import *
 import picamera
 from time import sleep
+import cv2
+import socket 
+import io 
 
 app = Flask(__name__)
 brightPi = BrightPi()
 brightPi.reset()
 camera = picamera.PiCamera()
+vc = cv2.VideoCapture(0) 
+
 
 @app.route('/')
 def index():
-    return "Hello, World!"
+    """Video streaming .""" 
+   return render_template('index.html') 
 
 @app.route('/photo/<option>')
 def takePicture(option):
@@ -24,6 +30,24 @@ def takePicture(option):
     resp = jsonify(success=True)
     resp.status_code = 200
     return resp
+    
+def gen(): 
+   """Video streaming generator function.""" 
+   while True: 
+       rval, frame = vc.read() 
+       cv2.imwrite('pic.jpg', frame) 
+       yield (b'--frame\r\n' 
+              b'Content-Type: image/jpeg\r\n\r\n' + open('pic.jpg', 'rb').read() + b'\r\n') 
+
+@app.route('/video_feed') 
+def video_feed(): 
+   """Video streaming route. Put this in the src attribute of an img tag.""" 
+   return Response(gen(), 
+                   mimetype='multipart/x-mixed-replace; boundary=frame') 
 
 if (__name__ == '__main__'):
     app.run(host='0.0.0.0', port=80)
+
+
+
+

742ab1bad7ad9be8b14b152ac69e1b7137a48bdd
jalmada <jalmada.e@gmail.com>
2019-12-04 23:07:57 -0500

    Again

--- server3.py
+++ server3.py
@@ -85,13 +85,13 @@ def gain():
     data = request.get_json()
     currentGain = brightPi.get_gain()
 
-    if(currentGain == 0 or currentGain == BrightPi._max_gain):
-        resp = jsonify(currentGain=currentGain, success=True)
-        resp.status_code = 200
-        return resp
-
     if (request.method == 'POST'):
         direction = data["direction"]
+
+        if((currentGain == 0 and direction == 'down') or (currentGain == BrightPi._max_gain and and direction == 'up')):
+            resp = jsonify(currentGain=currentGain, success=True)
+            resp.status_code = 200
+            return resp
 
         if(direction == "up"):
             currentGain = currentGain + 1
@@ -109,13 +109,15 @@ def dim():
     data = request.get_json()
     currentDim = brightPi.get_led_dim()[0]
 
-    if(currentDim == 0 or currentDim == BrightPi._max_dim):
-        resp = jsonify(currentDim=currentDim, success=True)
-        resp.status_code = 200
-        return resp
+   
 
     if (request.method == 'POST'):
         direction = data["direction"]
+
+        if((currentDim == 0 and direction == 'down') or (currentDim == BrightPi._max_dim and and direction == 'up')):
+            resp = jsonify(currentDim=currentDim, success=True)
+            resp.status_code = 200
+            return resp
 
         if(direction == "up"):
             currentDim = currentDim + 1

6502bde6fcffad66aa9b1c02e35d04803e327c64
Jose L. Almada <jalmada.e@gmail.com>
2019-12-07 01:28:24 -0500

    Testing Audio

--- server3.py
+++ server3.py
@@ -6,13 +6,13 @@ import io
 import logging
 from threading import Condition
 from datetime import datetime
-#import pyaudio
+import pyaudio
 
-# FORMAT = pyaudio.paInt16
-# CHANNELS = 2
-# RATE = 44100
-# CHUNK = 1024
-# RECORD_SECONDS = 5
+FORMAT = pyaudio.paInt16
+CHANNELS = 2
+RATE = 44100
+CHUNK = 1024
+RECORD_SECONDS = 5
 
 class StreamingOutput(object):
     def __init__(self):
@@ -42,24 +42,24 @@ output = StreamingOutput()
 camera.start_recording(output, format='mjpeg')
 currentLedDim = 0
 currentLedGain = 0
-#audio1 = pyaudio.PyAudio()
+audio1 = pyaudio.PyAudio()
 
-# def genHeader(sampleRate, bitsPerSample, channels):
-#     datasize = 2000*10**6
-#     o = bytes("RIFF",'ascii')                                               # (4byte) Marks file as RIFF
-#     o += (datasize + 36).to_bytes(4,'little')                               # (4byte) File size in bytes excluding this and RIFF marker
-#     o += bytes("WAVE",'ascii')                                              # (4byte) File type
-#     o += bytes("fmt ",'ascii')                                              # (4byte) Format Chunk Marker
-#     o += (16).to_bytes(4,'little')                                          # (4byte) Length of above format data
-#     o += (1).to_bytes(2,'little')                                           # (2byte) Format type (1 - PCM)
-#     o += (channels).to_bytes(2,'little')                                    # (2byte)
-#     o += (sampleRate).to_bytes(4,'little')                                  # (4byte)
-#     o += (sampleRate * channels * bitsPerSample // 8).to_bytes(4,'little')  # (4byte)
-#     o += (channels * bitsPerSample // 8).to_bytes(2,'little')               # (2byte)
-#     o += (bitsPerSample).to_bytes(2,'little')                               # (2byte)
-#     o += bytes("data",'ascii')                                              # (4byte) Data Chunk Marker
-#     o += (datasize).to_bytes(4,'little')                                    # (4byte) Data size in bytes
-#     return o
+def genHeader(sampleRate, bitsPerSample, channels):
+    datasize = 2000*10**6
+    o = bytes("RIFF",'ascii')                                               # (4byte) Marks file as RIFF
+    o += (datasize + 36).to_bytes(4,'little')                               # (4byte) File size in bytes excluding this and RIFF marker
+    o += bytes("WAVE",'ascii')                                              # (4byte) File type
+    o += bytes("fmt ",'ascii')                                              # (4byte) Format Chunk Marker
+    o += (16).to_bytes(4,'little')                                          # (4byte) Length of above format data
+    o += (1).to_bytes(2,'little')                                           # (2byte) Format type (1 - PCM)
+    o += (channels).to_bytes(2,'little')                                    # (2byte)
+    o += (sampleRate).to_bytes(4,'little')                                  # (4byte)
+    o += (sampleRate * channels * bitsPerSample // 8).to_bytes(4,'little')  # (4byte)
+    o += (channels * bitsPerSample // 8).to_bytes(2,'little')               # (2byte)
+    o += (bitsPerSample).to_bytes(2,'little')                               # (2byte)
+    o += bytes("data",'ascii')                                              # (4byte) Data Chunk Marker
+    o += (datasize).to_bytes(4,'little')                                    # (4byte) Data size in bytes
+    return o
 
 
 @app.route('/')
@@ -200,28 +200,28 @@ def gen():
     except Exception as e:
         logging.warning(e)
 
-# @app.route('/audio')
-# def audio():
-#     # start Recording
-#     def sound():
+@app.route('/audio')
+def audio():
+    # start Recording
+    def sound():
 
-#         CHUNK = 1024
-#         sampleRate = 44100
-#         bitsPerSample = 16
-#         channels = 2
-#         wav_header = genHeader(sampleRate, bitsPerSample, channels)
+        CHUNK = 1024
+        sampleRate = 44100
+        bitsPerSample = 16
+        channels = 2
+        wav_header = genHeader(sampleRate, bitsPerSample, channels)
 
-#         stream = audio1.open(format=FORMAT, channels=CHANNELS,
-#                         rate=RATE, input=True,input_device_index=1,
-#                         frames_per_buffer=CHUNK)
-#         print("recording...")
-#         #frames = []
+        stream = audio1.open(format=FORMAT, channels=CHANNELS,
+                        rate=RATE, input=True,input_device_index=1,
+                        frames_per_buffer=CHUNK)
+        print("recording...")
+        #frames = []
 
-#         while True:
-#             data = wav_header+stream.read(CHUNK)
-#             yield(data)
+        while True:
+            data = wav_header+stream.read(CHUNK)
+            yield(data)
 
-#     return Response(sound())
+    return Response(sound())
 
 
 

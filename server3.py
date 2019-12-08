1b7270ffd34f9a520a413bbd53b4d51f9e381d34
Jose L. Almada <jalmada.e@gmail.com>
2019-12-08 18:06:04 -0500

    Meh again

--- server3.py
+++ server3.py
@@ -218,7 +218,7 @@ def audio():
             data = wav_header+stream.read(CHUNK)
             yield(data)
             
-    return Response(sound(), mimetype="audio/x-wav; boundary=frame")
+    return Response(sound(), mimetype="audio/x-wav")
 
 
 

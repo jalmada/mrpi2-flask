a1bb8da1d1de4323f1f0084e50f6f572d38d78c3
Jose L. Almada <jalmada.e@gmail.com>
2019-12-08 19:09:58 -0500

    asd

--- server3.py
+++ server3.py
@@ -215,7 +215,7 @@ def sound():
 
 @app.route('/audio')
 def audio():   
-    return Response(sound(),  mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(sound(),  mimetype='audio/x-wav; boundary=frame')
 
 
 

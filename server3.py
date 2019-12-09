6fab059e231a4820655f580e96018589d3fb2857
Jose L. Almada <jalmada.e@gmail.com>
2019-12-08 20:10:42 -0500

    asdas

--- server3.py
+++ server3.py
@@ -38,29 +38,12 @@ currentLedDim = 0
 currentLedGain = 0
 audio1 = pyaudio.PyAudio()
 
-FORMAT = pyaudio.paFloat32
+FORMAT = pyaudio.paInt32
 CHANNELS = 1
 RATE = 44100
 CHUNK = 512
 RECORD_SECONDS = 5
 BITS_PER_SAMPLE = 16
-
-# def genHeader(sampleRate, bitsPerSample, channels):
-#     datasize = 2000*10**6
-#     o = bytes("RIFF",'ascii')                                               # (4byte) Marks file as RIFF
-#     o += (datasize + 36).to_bytes(4,'little')                               # (4byte) File size in bytes excluding this and RIFF marker
-#     o += bytes("WAVE",'ascii')                                              # (4byte) File type
-#     o += bytes("fmt ",'ascii')                                              # (4byte) Format Chunk Marker
-#     o += (16).to_bytes(4,'little')                                          # (4byte) Length of above format data
-#     o += (1).to_bytes(2,'little')                                           # (2byte) Format type (1 - PCM)
-#     o += (channels).to_bytes(2,'little')                                    # (2byte)
-#     o += (sampleRate).to_bytes(4,'little')                                  # (4byte)
-#     o += (sampleRate * channels * bitsPerSample // 8).to_bytes(4,'little')  # (4byte)
-#     o += (channels * bitsPerSample // 8).to_bytes(2,'little')               # (2byte)
-#     o += (bitsPerSample).to_bytes(2,'little')                               # (2byte)
-#     o += bytes("data",'ascii')                                              # (4byte) Data Chunk Marker
-#     o += (datasize).to_bytes(4,'little')                                    # (4byte) Data size in bytes
-#     return o
 
 def genHeader(sampleRate, bitsPerSample, channels):
     datasize = 2000*10**6

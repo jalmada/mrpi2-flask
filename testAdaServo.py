0f782f87dc7724a8da6b2ae14f131fc8250f8dca
Jose Almada <jalmada.e@gmail.com>
2020-02-05 21:13:21 -0500

    Freexe

--- testAdaServo.py
+++ testAdaServo.py
@@ -6,10 +6,12 @@ from adafruit_servokit import ServoKit
 try:
 	kit = ServoKit(channels=16,address=0x41)
 	kit.servo[0].angle = 180
-	kit.continuous_servo[1].throttle = 1
+	kit.servo[1].angle = 0
+	#kit.continuous_servo[1].throttle = 1
 	time.sleep(1)
-	kit.continuous_servo[1].throttle = -1
-	time.sleep(1)
-	kit.servo[0].angle = 0
+	#kit.continuous_servo[1].throttle = -1
+	#time.sleep(1)
+	#kit.servo[0].angle = 0
+	#kit.servo[1].angle = 150
 except Exception as e:
 	print(e)

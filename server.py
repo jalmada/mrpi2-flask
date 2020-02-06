2d3710c30433084f6a3fba77859b6d6e2ce2f1c9
Jose L. Almada <jalmada.e@gmail.com>
2020-02-05 23:45:15 -0500

    last

--- server.py
+++ server.py
@@ -28,6 +28,7 @@ thread_lock = Lock()
 
 @app.after_request
 def after_request(response):
+  response.headers.add('Access-Control-Allow-Origin', '*')
   response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
   response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
   response.headers.add('Access-Control-Allow-Credentials', 'true')

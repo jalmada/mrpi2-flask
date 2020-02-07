3cceca4005f92aac8a79312ae6619e2d74bb146a
Jose L. Almada <jalmada.e@gmail.com>
2020-02-07 00:11:55 -0500

    Removed response

--- server.py
+++ server.py
@@ -26,13 +26,7 @@ socketio = SocketIO(app, async_mode=async_mode, cors_allowed_origins="*")
 thread = None
 thread_lock = Lock()
 
-@app.after_request
-def after_request(response):
-  response.headers.add('Access-Control-Allow-Origin', '*')
-  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-  response.headers.add('Access-Control-Allow-Credentials', 'true')
-  return response
+
 
 @app.route('/')
 def index():
